dbe7605c33f856e249b535fcd019d94ed7786adf
Teemu Laurila <laurilateemu8@gmail.com>
2021-08-05 09:19:23 +0300

    Create cameraStream.py

---
+++ cameraStream.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, Response
+import cv2
+import pyautogui
+import numpy as np
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)  # use 0 for web camera
+
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        suc, img = camera.read()
+
+        # HIGH RES BUT SLOW?
+        #img = cv2.resize(img, ((480,320)), interpolation=cv2.INTER_AREA)
+        #img = cv2.resize(img, ((480,480)), interpolation=cv2.INTER_AREA)
+        img = cv2.resize(img, ((240,180)), interpolation=cv2.INTER_NEAREST)    #1.8.2021
+        #img = cv2.resize(img, ((120,90)), interpolation=cv2.INTER_AREA)
+        #img = cv2.resize(img, ((60,45)), interpolation=cv2.INTER_NEAREST)
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type:image/jpeg\r\n'
+               b'Content-Length: ' + f"{len(frame)}".encode() + b'\r\n'
+               b'\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video')
+def video():
+    page = """
+    <html>
+        <head>
+            <title>Video Streaming Demonstration</title>
+        </head>
+        <body style="background-color:black;">
+            <img id="stream" heigth="100%" width="100%" src="http://10.0.0.178:5000/video_feed" onerror="javascript: alert('failure')"/>
+        </body>
+    
+    <script>
+        let stateCheck = setInterval(() => {
+        if (document.readyState === 'complete') {
+            clearInterval(stateCheck);
+            // document ready
+        }
+        console.log(document.readyState);
+        }, 100);
+    
+    </script>
+    
+    </html>
+    """
+
+    return page
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0")
